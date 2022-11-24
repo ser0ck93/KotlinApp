3b1a06a9f747d12791f72fca7bf4de226ed9eda8
sebasmintic93 <sebasmintic93@gmail.com>
2022-11-24 01:35:18 -0500

    aplicación version 4

--- app/src/main/java/com/example/prueba/SpecialistFragment.kt
+++ app/src/main/java/com/example/prueba/SpecialistFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.prueba.databinding.FragmentLocationBinding
@@ -21,6 +22,7 @@ class SpecialistFragment : Fragment() {
     private val binding: FragmentSpecialistBinding get() = _binding!!
     private val args: SpecialistFragmentArgs by navArgs()
     private lateinit var doctorAdapter: DoctorAdapter
+    private lateinit var categories: MutableList<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,15 +42,41 @@ class SpecialistFragment : Fragment() {
             adapter=doctorAdapter
             layoutManager=LinearLayoutManager(requireContext())
         }
-        val specialists= listOf(
-            DoctorItemModel("1","Dr. Sebastian Toro","Odontologia","+300",R.drawable.doc.toString(),4,"el mejor en lo que hace")
+
+        val services = listOf(
+            ServiceitemModel( "1","General","los mejores especialistas en medicina general",R.drawable.cruz.toString()),
+            ServiceitemModel( "1","Especialista","los mejores especialistas",R.drawable.cruz.toString()),
+            ServiceitemModel( "1","Dermatologia","los mejores especialistas en Dermatologia",R.drawable.cura.toString()),
+            ServiceitemModel( "1","Odontologia","los mejores especialistas en Odontologia",R.drawable.diente.toString())
         )
+        categories=services.map{it.title}.toMutableList()
+        categories.add(0,"Todos")
+
+        val specialists = listOf(
+            DoctorItemModel("1","Dr. Sebastian Toro","Odontologia","+100",R.drawable.doc.toString(),2,"el mejor en lo que hace"),
+            DoctorItemModel("1","Dr. Manuel Camargo","General","+200",R.drawable.doc.toString(),2,"el mejor en lo que hace"),
+            DoctorItemModel("1","Dra. Nayive Chiappe","Especialista","+300",R.drawable.doc.toString(),1,"el mejor en lo que hace"),
+            DoctorItemModel("1","Dra. Yireth Fonseca","Odontologia","+250",R.drawable.doc.toString(),3,"el mejor en lo que hace"),
+            DoctorItemModel("1","Dr. Nelson Cipa","General","+180",R.drawable.doc.toString(),2,"el mejor en lo que hace"),
+            DoctorItemModel("1","Dr. Sebastian castro","Dermatologia","+350",R.drawable.doc.toString(),1,"el mejor en lo que hace"
+        )
+        )
+
+
         if(args.search){
             binding.specialistFragmentRecyclerSearchLayout.visibility = View.VISIBLE
             binding.specialistFragmentRecyclerTitle.visibility = View.GONE
             binding.specialistFragmentRecyclerTitle.text = getString(R.string.specialist_title)
             binding.specialistFragmentSubtitle.text = getString(R.string.specialist_subtitle)
             doctorAdapter.updateDataset(specialists)
+            binding.specialistFragmentRecyclerSearch.setAdapter(ArrayAdapter(requireContext(),android.R.layout.simple_dropdown_item_1line,categories))
+            binding.specialistFragmentRecyclerSearch.setOnItemClickListener { parent, view, position, id ->
+                if(position==0){
+                    doctorAdapter.updateDataset(specialists)
+                }else{
+                val category = categories[position]
+                doctorAdapter.updateDataset(specialists.filter { it.speciality == category })
+            }}
         }else{
             binding.specialistFragmentRecyclerSearchLayout.visibility = View.GONE
             binding.specialistFragmentRecyclerSearch.visibility = View.VISIBLE
